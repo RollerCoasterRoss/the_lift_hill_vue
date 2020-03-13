d5b94441dc90730708e04701d5005d47a60dcccf
RollerCoasterRoss <rsw3020@gmail.com>
2020-03-13 15:10:53 -0500

    initial Vue setup and AttractionReviews index page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
-<template>
+<template> 
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/attraction-review">Attraction Reviews</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/AttractionReviewsIndex.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="attraction-review">
+    <h1>Attraction Reviews</h1>
+    <div v-for="attraction_review in attraction_reviews">
+      <h3>Fear Factor: {{ attraction_review.fear_factor }}</h3>
+      <h3>Fun Factor: {{ attraction_review.fun_factor }}</h3>
+      <p>Comments: {{ attraction_review.body }}</p>
+      <p>User ID: {{ attraction_review.user_id }}</p>
+      <p>Attraction ID: {{ attraction_review.attraction_id }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+var axios = require('axios');
+
+export default {
+  data: function() {
+    return {
+      attraction_reviews: []
+    };
+  },
+  created: function() {
+    axios
+      .get("/api/attraction_reviews")
+      .then(response => {
+        this.attraction_reviews = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
